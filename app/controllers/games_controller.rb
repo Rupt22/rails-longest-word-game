7e99efa59912b8a7584c3a5e676d5ea29e801f68
rupert sealey <rupertsealey@hotmail.com>
2022-08-22 17:58:49 +0100

    35% completed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,26 @@
 class GamesController < ApplicationController
   def new
-    @letters = ('A'..'Z').to_a.shuffle.take(10)
+    @letters = []
+    10.times do
+     @letters << [*'a'..'z'].sample
+    end
   end
 
   def score
-   #raise
+   word = params[:word]
+   letters = JSON.parse(params[:letters])
+   guess_letters = word.chars
+   @count = guess_letters.all? {|letter| word.count(letter) <= letters.count(letter) }
+   if @count
+    valid_word_check = URI.open("https://wagond-dictionary.herokuapp.com/#{word}")
+     is_valid = JSON.parse(valid_word_check) ["found"]
+     if is_valid
+        @result = "Horray you picked a vlaid word"
+     else
+        @result = "sorry, #{word} is not an english word"
+     end
+   else
+     @result = "sorry you can't build #{word} with these letters"
+   end
   end
 end
